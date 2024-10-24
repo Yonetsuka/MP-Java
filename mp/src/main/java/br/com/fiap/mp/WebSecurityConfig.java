adba700431cdd160a62b00fee7596fa2174500fb
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-10-24 08:38:09 -0300

    Create WebSecurityConfig.java

---
+++ mp/src/main/java/br/com/fiap/mp/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+import br.com.fiap.mp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig{
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+    http.authorizeHttpRequests((requests) -> requests.anyrequest().authenticated()).httpBasic(Customizer.withDefaults());
+
+    return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailService(){
+    UserDetails user = User.withDefaultPasswordEncoder().username("lerolero").password("12345").roles("ADM").build();
+    return new InMemoryUserDetailsManager(user);
+  }
+}
